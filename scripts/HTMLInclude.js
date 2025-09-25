b4429f1f2f8e31528f0227d9e65cae1e4a611cee
thomas <tommyb8586@gmail.com>
2025-09-24 20:21:48 -0400

    Dynamically loaded footer and header and some other various changes

---
+++ scripts/HTMLInclude.js
@@ -0,0 +1,18 @@
+function includeHTML() {
+    const elements = document.querySelectorAll('[include-html]');
+    elements.forEach(el => {
+        const file = el.getAttribute('include-html');
+        if (file) {
+            fetch(file)
+                .then(response => response.text())
+                .then(data => {
+                    el.innerHTML = data;
+                })
+                .catch(err => {
+                    el.innerHTML = "Content not found.";
+                    console.error("Include Error: ", err);
+                });
+            }
+        });
+}
+document.addEventListener("DOMContentLoaded", includeHTML);
